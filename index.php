5e7c67ce89a8b5aab1d43f8f5c66bd19be4b8556
JEkstroms <jmekstrom@gmail.com>
2015-08-25 12:09:44 -0700

    landing page

--- index.php
+++ index.php
@@ -1,2 +1,61 @@
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<!-- Latest compiled JavaScript -->
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="bootstrap-social.css">
 
-<img src="http://www.photosinbox.com/download/blank-notepad.jpg" alt="">
+<div class="container-fuild">
+    <div class="header col-xs-12">
+        <div class="title pull-left">
+            <h1>Todo List</h1>
+        </div>
+        <button class="btn btn-default pull-right login">Login</button>
+    </div>
+    <div class="content_container">
+        <div class="register">
+            <h2 class="page-header">Sign Up</h2>
+            <form role="form">
+                <button class="btn btn-block btn-social btn-facebook facebook">
+                    <i class="fa fa-facebook"></i> Register with Facebook
+                </button>
+                <button class="btn btn-block btn-social btn-twitter">
+                    <i class="fa fa-twitter"></i> Register with Twitter
+                </button>
+                <button class="btn btn-block btn-social btn-google">
+                    <i class="fa fa-google"></i> Register with Google
+                </button>
+                <div class="page-header space">
+                </div>
+                <div class="form-group">
+                    <label for="email">Email address:</label>
+                    <input type="email" class="form-control" id="email">
+                </div>
+                <div class="form-group">
+                    <label for="pwd">Password:</label>
+                    <input type="password" class="form-control" id="pwd">
+                </div>
+
+                <button type="submit" class="btn btn-success">Register</button>
+            </form>
+        </div>
+    </div>
+    <div class="about">
+        <h1>
+            About
+        </h1>
+
+        <p>adv.
+            Old English abutan, earlier onbutan "on the outside of," from on (see on ) + be "by" (see by ) + utan
+            "outside," from ut (see out (adv.)). By 13c. it had forced out Old English ymbe, ymbutan for meaning "in
+            the neighborhood of." Abouts, with adverbial genitive, still found in hereabouts, etc., probably is a
+            northern dialectal form. About face as a military command (short for right about face) is first attested
+            1861, American English.</p>
+    </div>
+    <div class="footer col-xs-12">
+        <p>If you have any questions email me at daniel.paschal@learningfuze.com </p>
+    </div>
+</div>
