0a889693a66f096f6426b9520c3a05c6eae8ba58
JEkstroms <jmekstrom@gmail.com>
2015-08-28 10:22:13 -0700

    idk

--- main.js
+++ main.js
@@ -19,9 +19,7 @@ function login_ajaxCall() {
                 load_content("login");
             }
         },
-        error: function(data){
-            console.log(data);
-        }
+
     })
 }
 
@@ -52,25 +50,75 @@ function logout_ajaxCall(){
     })
 }
 
-var todo_item_array;
+var todo_item_array=[];
 var id = 0;
 var modal_td_DOM = $("<td>",{
     "data-toggle": "modal",
     "data-target": "itemModal"
 })
-var checkbox = $("<input>",{
-    type: "checkbox"
-})
+
 function addTask(){
     id++;
     var task     = $("#taskInput").val();
     var date     = $("#dateInput").val();
     var priority = $("#priorityInput").val();
     var details  = $("#detailsInput").val();
-    var timeStamp = new Date().getTime();
-    var todo_item = {id:id,task:task,date:date,priority:priority,details:details,timeStamp:timeStamp}
-    //todo_item_array.push(todo_item);
-    $(".tableBottom").before("<tr id='"+id+"'><td><input type='checkbox'></td>"+modal_td_DOM+"</tr>");
+    var created_datetime = new Date().getTime();
+    var complete = "incomplete";
+    var todo_item = {id:id,complete:complete,task:task,date:date,priority:priority,details:details,created_datetime:created_datetime}
+    todo_item_array.push(todo_item);
+   var $tableRow = $('<tr>',{
+       id: id,
+       'data-index': id
+   })
+    var $checkbox_td = $('<td>',{
+        id: "checkbox_td"
+    })
+    var $checkbox = $('<input>',{
+        type: "checkbox",
+        value: "complete"
+    })
+    var $task_td = $("<td>", {
+        id: "task_td",
+        onclick: "showTask("+id+")"
+    }).text(task);
+    var $date_td = $("<td>", {
+        id: "date_td",
+        onclick: "showTask("+id+")"
+    }).text(date);
+    var $priority_td = $("<td>", {
+        id: "priority_td",
+        onclick: "showTask("+id+")"
+    }).text(priority);
+    $($checkbox_td).append($checkbox);
+    $($tableRow).append($checkbox_td,$task_td,$date_td,$priority_td);
+    $('.tableBottom').before($tableRow);
 
 }
 
+function addClicked(){
+    $('input').val('');
+    $('select').val('');
+    $('textarea').val('');
+    $('#addModal').modal('show');
+}
+
+function showTask(id){
+    for(var i in todo_item_array){
+        if(todo_item_array[i].id == id){
+            var todoObj = todo_item_array[i];
+        }
+    }
+    if(todoObj === undefined) {
+        console.log("something is wrong with me...");
+        return;
+    }
+    $("#task").html("Task: "+todoObj.task);
+    $("#status_p").html("Status:" + todoObj.complete);
+    $("#date_p").html("Complete by: " + todoObj.date);
+    $("#priority_p").html("Priority: " + todoObj.priority);
+    $("#details_p").html("Extra Details: " + todoObj.details);
+    $("#created_p").html("Created: " + todoObj.created_datetime);
+    $("#id_p").html("Task Number: " + todoObj.id);
+    $('#itemModal').modal('show');
+}
--- pages/list_all_items.php
+++ pages/list_all_items.php
@@ -4,31 +4,13 @@
         <th class="checkbox_th">✓</th>
         <th>Task
             <button class="addBtn btn btn-xs btn-success" type="button"
-                data-toggle="modal" data-target="#addModal">+</button>
+                onclick="addClicked()">+</button>
         </th>
         <th>Date</th>
         <th>Priority</th>
     </tr>
     </thead>
     <tbody>
-    <tr>
-        <td><input type="checkbox"></td>
-        <td data-toggle="modal" data-target="#itemModal">Creating version 0.1 of the to do list</td>
-        <td data-toggle="modal" data-target="#itemModal">8/25/15</td>
-        <td data-toggle="modal" data-target="#itemModal">high</td>
-    </tr>
-    <tr>
-        <td><input type="checkbox"></td>
-        <td>Finish the to do list</td>
-        <td>9/11/15</td>
-        <td></td>
-    </tr>
-    <tr>
-        <td><input type="checkbox"></td>
-        <td>Start working on version 0.2</td>
-        <td></td>
-        <td></td>
-    </tr>
     <tr class="tableBottom">
         <td></td>
         <td></td>
@@ -89,12 +71,16 @@
                 <h4 class="modal-title">Task Details</h4>
             </div>
             <div class="modal-body">
-                <p>Task:</p>
-                <p>Date:</p>
-                <p>Priority:</p>
-                <p>Details:</p>
+                <h3 id="task"></h3>
+                <p id="status_p"></p>
+                <p id="date_p"></p>
+                <p id="priority_p"></p>
+                <p id="created_p"></p>
+                <p id="id_p"></p>
+                <p id="details_p"></p>
             </div>
             <div class="modal-footer">
+                <button type="button" class="btn btn-danger" data-dismiss="modal" onclick="deleteTask()">Delete</button>
             </div>
         </div>
 
