3de099cb6808bdb1d57840ebf2c0e67de4d2f186
Alex Mattingley <alexmattingley@gmail.com>
2015-09-02 15:55:46 -0700

    response is data for dom

--- data_handler_send.php
+++ data_handler_send.php
@@ -2,7 +2,7 @@
 session_start();
 
 if(!empty($_SESSION['user_id'])){
-    print($_POST["task_data"]["complete"]);
+    //print($_POST["task_data"]["complete"]);
     $complete = "0";
     $created_datetime = "0000-00-00";
     $details = "details of task";
@@ -16,12 +16,15 @@ if(!empty($_SESSION['user_id'])){
     $query = "INSERT INTO `tdl_item_list`(`id`, `task`, `details`, `created_datetime`, `user_id`, `complete`, `priority`, `due_date`) VALUES ('$id','$task','$details','$created_datetime','$user_id','$complete','$priority','$due_date')";
     $results = mysqli_query($conn, $query);
     if(mysqli_affected_rows($conn) > 0){
-        print('mysqli_affected_rows is true');
-        print_r($results);
+        $json_for_return = json_encode($_POST["task_data"]);
+        print($json_for_return);
+        //print('mysqli_affected_rows is true');
+        //print_r($results);
     }else {
         print('something is not working, its probably the id breaking things again.');
     }
 }else{
     print('you are either logged out or your credentials are wrong');
 }
+
 ?>
