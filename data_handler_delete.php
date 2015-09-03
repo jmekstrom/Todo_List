66a8c959e095582dadc36fddd8a8abb6f8006fb0
Alex Mattingley <alexmattingley@gmail.com>
2015-09-03 08:01:06 -0700

    static delete working, beginnings of dynamic data_handler #29

--- data_handler_delete.php
+++ data_handler_delete.php
@@ -1,19 +1,19 @@
 <?php
 
-session_start();
-$json_output = json_encode($_POST);
-print($json_output);
+    session_start();
+    $obj_id = $_POST["obj_id"];
 
-//require('mysql_connect.php');
-//$query = "";
-//$results = mysqli_query($conn, $query);
-//if(mysqli_num_rows($results) > 0){
-//    while($result = mysqli_fetch_assoc($results)){
-//        $user_info[] = $result;
-//    }
-//    $json_string = json_encode($user_info);
-//    print($json_string);
-//
-//}
+    require('mysql_connect.php');
+    $query = "DELETE FROM `tdl_item_list` WHERE id='4'";
+    $results = mysqli_query($conn, $query);
+    if(mysqli_affected_rows($conn) > 0){
+        $return["success"] = "You deleted the object with an id of: $obj_id";
+    }else {
+        $return["success"] = "something went wrong";
+
+    }
+
+    $json_for_return = json_encode($return);
+    print($json_for_return);
 
 ?>
