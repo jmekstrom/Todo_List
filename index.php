d96e346221d281dbd0b0f4a8ecafedea3a5e52a5
Alex Mattingley <alexmattingley@gmail.com>
2015-09-04 17:28:22 -0700

    headers are created for each of the individual pages. there might be a better way to do this, but this is the quick and dirty version.  closes #42

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 session_start();
-print_r($_SESSION);
 if (empty($_SESSION['user_id'])){
 
     $_GET['current_page'] = "login";
@@ -12,16 +11,7 @@ else{
 <?php
 include_once('template/header.php');
 ?>
-<div class="row header_div">
-    <div class='header col-xs-12'>
-        <div class='pull-left col-xs-6'>
-            <h1>Todo List</h1>
-        </div>
-        <div class="col-xs-6 account_summary">
-            <button class='btn btn-default pull-right' id="logoutBtn" onclick="logout_ajaxCall()">Log Out</button>
-        </div>
-    </div>
-</div>
+
 <div class="content_container">
     <?php
     include_once("pages/".$_GET['current_page'].".php");
--- pages/login.php
+++ pages/login.php
@@ -1,3 +1,13 @@
+<div class="row header_div">
+    <div class='header col-xs-12'>
+        <div class='pull-left col-xs-6'>
+            <h1>Todo List</h1>
+        </div> <!--End pull-left col-xs-6-->
+        <div class="col-xs-6 account_summary">
+            <button class='btn btn-default pull-right' id="createBtn" data-toggle="modal" data-target="#createmodal">Create Account</button>
+        </div>
+    </div> <!-- end header col-xs-12-->
+</div> <!--end row header_div-->
 <div class="register">
     <h2 class="page-header">Log in</h2>
 
@@ -23,5 +33,36 @@
         </div>
         <button type="button" class="btn btn-success" id="loginBtn" onclick="login_ajaxCall()">Login</button>
     </form>
+</div>
+
+
+
+<!-- Modal -->
+<div id="createmodal" class="modal fade" role="dialog">
+    <div class="modal-dialog">
+
+        <!-- Modal content-->
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">Create an Account</h4>
+            </div>
+            <div class="modal-body">
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Username" id="username">
+                </div>
+
+                <div class="form-group">
+                    <input type="password" class="form-control" placeholder="password" id="password">
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-success" data-dismiss="modal">
+                    Submit
+                </button>
+            </div>
+        </div>
+
+    </div>
 </div>
 
